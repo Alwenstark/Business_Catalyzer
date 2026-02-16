32773ec1a32f58d5329f000be1c571537038511a
alwen <alwenperara44@gmail.com>
2026-02-16 20:28:06 +0530

    first commit

---
+++ backend/src/main/java/com/businesscatalyzer/BusinessCatalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.businesscatalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessCatalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessCatalyzerApplication.class, args);
+	}
+
+}
